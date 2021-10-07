dc8018746490568869a68c82ef5288b7813fd041
Florian <Florianlebecque@hotmail.com>
2021-10-07 17:58:22 +0200

    Remove the moves finding logic

--- checkers/checker.js
+++ checkers/checker.js
@@ -51,7 +51,7 @@ class Checker{
             this.board[CurrentMove.middle_piece[0]][CurrentMove.middle_piece[1]] = null;
         }
 
-        if(CurrentMove.child !== null){
+        if(CurrentMove.child.length>0){
             this.Play(CurrentMove.child);
         }
 
@@ -64,22 +64,27 @@ class Checker{
         let mi = Math.floor(mouseX / this.grid_width);
         let mj = Math.floor(mouseY / this.grid_width);
 
-        let move_id = mi+"_"+mj;
-        if(this.moves.hasOwnProperty(move_id)){
-            this.Play(this.moves[move_id]);
+        let CurrentMove = this.FindMove([this.selectedPiece.i,this.selectedPiece.j],[mi,mj]);
+        
+        if(CurrentMove){
+            this.Play(CurrentMove);
         }
+            
+        
         
     }
 
-    CheckMove(target,possibilities){
-        let valide = false;
-        possibilities.forEach(p =>{
-            if((p[0] === target[0])&&(p[1]===target[1])){
-                valide = true;
-            }
-        })
+    FindMove(position,target){
 
-        return valide;
+        let moves = this.selectedPiece.GetPath();
+
+        for(let i = 0; i < moves.length;i++){
+            if(moves[i].to[0] === target[0]&&(moves[i].to[1]===target[1])){
+                return moves[i];
+            }
+        }
+        return null;
+
     }
 
     SelectPieces(){
@@ -109,104 +114,13 @@ class Checker{
     }
 
 
-    GetMoves(PathTable,team,position,dir,currentPath = null){
-        //check that we don't go out of the table
-        if(((position[1]+dir) < 8) && ((position[1]+dir) > -1)){
-            
-            //Get the neighbour position
-            let nPos = this.GetNeighbourPos(position,dir);
 
-            for(let i = 0; i < nPos.length;i++){
-
-                let p = [nPos[i][0],nPos[i][1]];
-
-                //check if it's free or not
-                if(this.board[p[0]][p[1]]){  //it's an pieces
-
-                    if(this.board[p[0]][p[1]].team !== team){
-                        
-                        let di = p[0] - position[0];
-                        let dj = p[1] - position[1];
-
-                        let ni = p[0] + di;
-                        let nj = p[1] + dj;
-
-                        if(this.board[ni][nj]===null){
-
-                            let nPath = {
-                                origin:position,
-                                to:[ni,nj],
-                                middle_piece:p,
-                                child:null
-                            };
-
-                            if(currentPath!== null){
-                                this.AddToPath(nPath,currentPath);
-                                this.GetMoves(PathTable,team,[ni,nj],dir,currentPath);
-                            }else{
-                                this.GetMoves(PathTable,team,[ni,nj],dir,nPath);
-                            }
-
-                            //add to path and recursive
-                        }
-
-                    }
-
-                }else{  //it's free
-
-                    let nPath = {
-                        origin:position,
-                        to:p,
-                        child:null
-                    }
-
-                    //we need to add to the new path
-                    if(currentPath === null){
-                        
-                        PathTable[nPath.to[0]+"_"+nPath.to[1]] = nPath;
-                        //currentPath.child = nPath;  
-                    }else{
-                        this.AddToPath(nPath,currentPath);
-                        PathTable[currentPath.to[0]+"_"+currentPath.to[1]] = currentPath;
-                    }
-
-
-                }
-            }
-
-
-
-            return PathTable;
-
-
+    GetLastPosition(move){
+        if(move.child !== null){
+            return this.GetLastPosition(move.child);
+        }else{
+            return move.to;
         }
-
-    }
-
-    /**
-     * @returns Position of a possible neightbour on the left or right
-     * trans : left (-1) or right (+1) 
-     */
-    GetPossibleNeighbourPos(position,trans,dir){
-        if((position[0]+trans >= 0)&&(position[0]+trans<8)){
-            return [position[0]+trans,position[1] + dir];
-        }
-        return null;
-    }
-    /**
-     * @returns Position of all the possible neightbour (array)
-     */
-    GetNeighbourPos(position,dir){
-        let neighbourPos = [];
-        let left    = this.GetPossibleNeighbourPos(position,-1,dir);
-        let right   = this.GetPossibleNeighbourPos(position,1,dir);
-        if(left){
-            neighbourPos.push(left);
-        }
-        if(right){
-            neighbourPos.push(right);
-        }
-        return neighbourPos;
     }
 
     GetMove(piece){
@@ -225,17 +139,8 @@ class Checker{
             }
 
 
-            this.moves = this.GetMoves([],piece.team,[piece.i,piece.j],dir);
+            this.moves = this.selectedPiece.GetPath()//this.GetMoves([],piece.team,[piece.i,piece.j],dir,false);
             console.log(this.moves);
-        }
-    }
-
-    AddToPath(nPath,currentPath){
-
-        if(currentPath.child !== null){
-            currentPath.child = nPath;
-        }else{
-            this.AddToPath(nPath,currentPath);
         }
     }
 
@@ -245,13 +150,10 @@ class Checker{
             moves = this.moves;
         }
 
-        if(moves.length > 0){
-            moves.forEach(m =>{
-                fill(0,255,0,100);
-                rectMode(CORNER);
-                rect(m[0]*this.grid_width, m[1]*this.grid_width, this.grid_width, this.grid_width);
-            });
+        for(let i = 0 ; i < moves.length;i++){
+            moves[i].Display(this.grid_width);
         }
+
     }
 
     Display(){
