5b96f6d079e162efb2afd3bb13f566924492e919
Florian <Florianlebecque@hotmail.com>
2022-12-14 11:26:25 +0100

    Change table for game list

--- users/gameHisto.js
+++ users/gameHisto.js
@@ -1,12 +1,13 @@
 function GetGameList() {
-    let list_elem = document.querySelector('#GameList');
-    list_elem.innerHTML = "<h2>Game List</h2>"
+
+    let list_elem = document.querySelector('#games-list');
     db.gun.get('gamelist').map().once(game=>{
         
 
         if((game.id)&&(game.current)){
-            let line = document.createElement('li');
-            line.title = game.user_0 + " - "+ game.user_1;
+            let table_row = document.createElement('tr');
+
+            let name_col = document.createElement("td");
 
 
             if(game.user_1 == "empty"){
@@ -16,22 +17,21 @@ function GetGameList() {
 
             let end = "";
             if(game.is_ended){
-                end = " | ended"
+                end = "ended"
             }
-
 
             let turn = ""
             let btn_txt = "Play"
             if(game.current == db.user.is.pub){
-                turn = " | It's not your turn "
+                turn = "It's not your turn "
                 
             }else{
 
                 if((game.user_0 == db.user.is.pub)||(game.user_1==db.user.is.pub)){
-                    turn = " | It's your turn "
+                    turn = "It's your turn "
 
                 }else{
-                    turn = " | spectate"
+                    turn = "spectate"
                     btn_txt = "Spectate"
                 }
                 
@@ -40,25 +40,33 @@ function GetGameList() {
 
             if(game.user_0 == db.user.is.pub){
                 db.gun.user(game.user_1).once((u) => {
-                    line.textContent = u.alias + ' ' + u.pub.slice(0, 5) + " | "+game.id.slice(0,5)+turn+end;
+                    name_col.textContent = u.alias + ' ' + u.pub.slice(0, 5) + " | "+game.id.slice(0,5)
                 });
             }else{
                 db.gun.user(game.user_0).once((u) => {
-                    line.textContent = u.alias + ' ' + u.pub.slice(0, 5) + " | "+game.id.slice(0,5)+turn+end;
+                    name_col.textContent = u.alias + ' ' + u.pub.slice(0, 5) + " | "+game.id.slice(0,5)
                 });
             }
 
-            
+            let info_col = document.createElement("td");
+            info_col.textContent = turn+end;
 
-    
-            list_elem.appendChild(line);
-            
+            let play_col = document.createElement("td");
             let discuss_btn = document.createElement('button');
+            discuss_btn.classList.add("btn");
+            discuss_btn.classList.add("btn-outline-primary");
+
             discuss_btn.textContent = btn_txt;
             discuss_btn.addEventListener('click', (e) => {
                 document.location.href = "game.html?p="+game.id;
             });
-            list_elem.appendChild(discuss_btn);
+            play_col.appendChild(discuss_btn);
+
+
+            table_row.appendChild(name_col);
+            table_row.appendChild(info_col);
+            table_row.appendChild(play_col);
+            list_elem.appendChild(table_row);
         }
 
         
